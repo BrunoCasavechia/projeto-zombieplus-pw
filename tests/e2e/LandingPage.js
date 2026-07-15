3f57b3be41247ba3a4e827769269149b15606db9
Bruno <bruno.casavechia@gmail.com>
2026-07-15 16:26:21 -0300

    test: testes de cadastro de leads (landing page)

--- tests/e2e/LandingPage.js
+++ tests/e2e/LandingPage.js
@@ -1,12 +1,38 @@
-import {test} from '@playwright'
+import { test, expect } from '@playwright/test'
+import { timeStamp } from 'node:console'
 
 export class LandingPage {
     constructor(page) {
-        page = this.page
+        this.page = page
     }
 
+    async visit() {
+        await this.page.goto('http://localhost:3000/')
+    }
 
-    async botaoCadastroUsuario() {
-        await this.page.getByRole('button', {name: 'Aperte o play... se tiver coragem'}).click()
+    async botaoCadastroLeads() {
+        await this.page.getByRole('button', { name: /Aperte o play/ }).click()
+        await expect(this.page.getByRole('heading', { name: 'Fila de espera' })).toBeVisible()
+    }
+
+    async formulario(nome, email) {
+        await this.page.getByPlaceholder('Informe seu nome').fill(nome)
+        await this.page.getByPlaceholder('Informe seu email').fill(email)
+        await this.page.getByRole('button', { name: 'Quero entrar na fila!' }).click()
+    }
+    
+    async textoToastCadastro(){
+        const message = 'Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!'
+
+        await expect(this.page.locator('.toast')).toHaveText(message)
+        await expect(this.page.locator('.toast')).toBeHidden({ timeout: 5000 })
+    }
+
+    async textoErroCampoVazio() {
+        await expect(this.page.getByText('Campo obrigatório')).toBeVisible()
+    }
+
+    async textoErroEmailIncorreto() {
+        await expect(this.page.getByText('Email incorreto')).toBeVisible()
     }
 }
--- tests/pages/landingPage.spec.js
+++ tests/pages/landingPage.spec.js
@@ -1,14 +1,38 @@
-import {tests} from "@playwright/test";
+import { test, expect } from "@playwright/test";
 import { LandingPage } from "../e2e/LandingPage";
 
-test('Acessando botão para criar usuario', async ({page}) => {
+test('Leads - Cadastro válido', async ({ page }) => {
     const landingPage = new LandingPage(page)
-    await landingPage.botaoCadastroUsuario()
 
+    await landingPage.visit()
+    await landingPage.botaoCadastroLeads()
+    await landingPage.formulario('zombieplus', 'zombieplus@gmail.com')
+    await landingPage.textoToastCadastro()
 })
 
-// test('Cadastrando o usuario', async ({page}) => {
-//     const LandingPage = new Landing(page)
+test('Leads - Cadastro com campos em branco (nome)', async ({ page }) => {
+    const landingPage = new LandingPage(page)
 
-//     await 
-// })
+    await landingPage.visit()
+    await landingPage.botaoCadastroLeads()
+    await landingPage.formulario('', 'zombieplus@gmail.com')
+    await landingPage.textoErroCampoVazio()
+})
+
+test('Leads - Cadastro com campos em branco (email)', async ({ page }) => {
+    const landingPage = new LandingPage(page)
+
+    await landingPage.visit()
+    await landingPage.botaoCadastroLeads()
+    await landingPage.formulario('zombieplus', '')
+    await landingPage.textoErroCampoVazio()
+})
+
+test('Leads - Cadastro com campo de email inválido', async ({page}) => {
+    const landingPage = new LandingPage(page)
+
+    await landingPage.visit()
+    await landingPage.botaoCadastroLeads()
+    await landingPage.formulario('zombieplus', 'zombieplusgmail.com')
+    await landingPage.textoErroEmailIncorreto()
+})
